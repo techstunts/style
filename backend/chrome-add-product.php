8a37467f715cdea377b490989ea41001b2e7bac6
techstunts <techstunts@gmail.com>
2015-11-19 17:03:13 +0530

    Add product from koovs

---
+++ backend/chrome-add-product.php
@@ -0,0 +1,20 @@
+<?php
+include 'databaseconnect.php';
+
+		if(isset($_GET['name'])) {
+			$name	=	$_GET['name'];
+			$price		=	str_replace(array(","," "), "", $_GET['price']);
+			$image	=	$_GET['image0'];
+			$url		=	$_GET['url'];
+			
+			$insertQuery  	=	"INSERT INTO `lookdescrip` 
+			(`product_name`, `product_type`, `product_price`, `product_link`, `upload_image`, `image_name`) 
+			VALUES ( '$name', 'Dress', '$price', '$url', '$image', '$image')";
+			
+			$executeQuery	=	mysql_query($insertQuery);
+			$product_url = "http://istyleyou.loc/backend/list_style_item1.php?id=" . mysql_insert_id();
+			if($executeQuery){
+				echo  json_encode([true, $product_url]);
+			}
+		}
+?> 
---
+++ chrome/popup.js
@@ -0,0 +1,110 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var fetchButton = document.getElementById('fetch');
+      
+  fetchButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+			chrome.tabs.executeScript(
+				null,
+				{
+					code: '\
+					var prod_name = document.getElementById(\'prod_name_hide\').value;\
+					var prod_price = document.getElementsByClassName(\'product-price\')[0].getElementsByTagName(\'strong\')[0].innerHTML;\
+					var prod_desc = document.getElementById(\'product-desc\').innerText;\
+					var a_tags = document.getElementsByClassName(\'thumb-views\')[0].getElementsByTagName(\'a\');\
+					var img_links = [];\
+					for(var i=0; i<a_tags.length; i++){\
+						var rel = a_tags[i].rel.replace(/\",\"/g,", ");\
+						img_links.push(rel.split(\',\')[0].split(\'\":\"\')[1]);\
+					}\
+					var r = [prod_name, prod_price, prod_desc, img_links];\
+					r;\
+					'
+				},
+				function(results){ 
+					document.getElementById('name').value = results[0][0];
+					document.getElementById('price').value = results[0][1];
+					document.getElementById('desc').innerHTML = results[0][2];
+					document.getElementById('images').innerHTML = results[0][3];
+					document.getElementById('url').value = tab.url;
+					
+					var imagesDiv = document.getElementsByClassName('images')[0];
+
+					//Remove previously added image tags
+					var images = imagesDiv.getElementsByTagName('img');
+					while(images.length > 0){
+						images[0].parentNode.removeChild(images[0]);
+					}
+					
+					//Remove previously added image url input tags
+					var image_urls = imagesDiv.getElementsByClassName('prod_image');
+					while(image_urls.length > 0){
+						image_urls[0].parentNode.removeChild(image_urls[0]);
+					}
+					
+					
+					//Add image tags and image url input tags
+					for(cnt in results[0][3]){
+						var image = document.createElement('img');
+						image.src=results[0][3][cnt];
+						imagesDiv.appendChild(image);
+						
+						var input = document.createElement('input');
+						input.value = results[0][3][cnt];
+						input.name = "image" + cnt;
+						input.className = "prod_image";
+						imagesDiv.appendChild(input);
+					}
+					
+				}
+			);
+
+
+    });
+  }, false);
+  
+  var saveButton = document.getElementById('save');
+  
+  saveButton.addEventListener('click', function() {
+
+
+			var kvpairs = [];
+			var input_elements = document.getElementsByTagName('input');
+			for (var i = 0; i < input_elements.length; i++ ) {
+				 var e = input_elements[i];
+				 kvpairs.push(encodeURIComponent(e.name) + "=" + encodeURIComponent(e.value));
+			}
+			var queryString = kvpairs.join("&");
+
+			var url = "http://istyleyou.loc/backend/style/chrome-add-product.php";
+			url = url + "?" + queryString;
+			//alert(url);
+			console.log(url);
+			
+			var xmlDoc = new XMLHttpRequest();
+			xmlDoc.open('GET', url, true);
+
+			xmlDoc.onreadystatechange = function() {
+				if (xmlDoc.readyState === 4 && xmlDoc.status === 200) {
+					console.log(xmlDoc.responseText);
+					console.log(xmlDoc.responseText[0]);
+					var response = JSON.parse(xmlDoc.responseText);
+					if(response[0])
+					{
+						var alink = document.createElement('a');
+						alink.href = response[1];
+						alink.text = 'Check product';
+						alink.target = 'New';
+						saveButton.parentNode.appendChild(alink); 
+						alert("Product saved");
+					}
+				}
+			}
+
+			xmlDoc.send();
+			
+	}, false);
+	
+}, false);
