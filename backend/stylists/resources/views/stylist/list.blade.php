5b4aed2f71601437751d860e5b96b314df0701a7
danishahmed73 <danishahmed73@gmail.com>
2016-04-29 11:36:01 +0530

    Stylist List blade changes.

--- backend/stylists/resources/views/stylist/list.blade.php
+++ backend/stylists/resources/views/stylist/list.blade.php
@@ -3,41 +3,98 @@
 @section('title', 'Stylist list')
 
 @section('content')
-<div id="contentCntr">
-    <div class="section">
-        <div class="container">
-            {!! $stylists->render() !!}
-
-            <div class="clear"></div>
-
-            <ol class="selectable" >
-            @foreach($stylists as $stylist)
-                <li class="ui-state-default" stylist_id="{{$stylist->stylish_id}}">
-                    <div class="items">
-                        <div class="name text"><a href="{{url('stylist/view/' . $stylist->stylish_id)}}">{{$stylist->name}}</a></div>
-                        <div class="image"><img src="{!! strpos($stylist->image, "stylish") === 0 ? asset('images/' . $stylist->image) : $stylist->image !!}" /></div>
-                        <div class="extra text">
-                            <span>{{$status_list[$stylist->status_id]->name}}</span>
-                            <span>{{$stylist->expertise->name}}</span>
-                            <span>{{$stylist->age}}</span>
-                            <span>{{$stylist->gender->name}}</span>
-                        </div>
-                        <div class="name text">{{$stylist->description}}</div>
+    <div id="contentCntr">
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2">
+                        <form action="/stylist/list" method="get">
+                            <select onchange="this.form.submit()" name="designation_id" id="designation">
+                                <option value="" disabled>Select Designations</option>
+                                <option value="1">Stylist</option>
+                                <option value="3">Team</option>
+                            </select>
+                        </form>
                     </div>
-                </li>
-            @endforeach
-            </ol>
 
-            <div class="clear"></div>
+                    <div class="col-md-2">
+                        <form action="/stylist/list" method="get">
+                            <input type="text" id="searchname" name="searchname" placeholder="search stylist here">
+                        </form>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4"></div>
+                    <div class="col-lg-4 text-center">
+                        {!! $stylists->render() !!}
+                    </div>
+                    <div class="col-lg-4"></div>
+                </div>
 
-            {!! $stylists->render() !!}
+                <br>
+                <div class="clear"></div>
+                <ol class="selectable">
+                    @foreach($stylists as $stylist)
+                        <li class="ui-state-default" stylist_id="{{$stylist->stylish_id}}">
+                            <div class="items">
+                                <div class="name text"><a
+                                            href="{{url('stylist/view/' . $stylist->stylish_id)}}">{{$stylist->name}}</a>
+                                </div>
+                                <div class="image"><img
+                                            src="{!! strpos($stylist->image, "stylish") === 0 ? asset('images/' . $stylist->image) : $stylist->image !!}"/>
+                                </div>
+                                <div class="extra text">
+                                    <span>{{$status_list[$stylist->status_id]->name}}</span>
+                                    <span>{{$stylist->expertise->name}}</span>
+                                    <span>{{$stylist->age}}</span>
+                                    <span>{{$stylist->gender->name}}</span>
+                                </div>
+                                <div class="name text">{{$stylist->description}}</div>
+                            </div>
+                        </li>
+                    @endforeach
+                </ol>
+
+                <div class="clear"></div>
+
+                {!! $stylists->render() !!}
+
+            </div>
+
+
+            @include('look.create')
 
         </div>
-
-
-        @include('look.create')
-
     </div>
-</div>
+    {{--<script src="js/designation.js"></script>--}}
+    <script type="text/javascript">
+        jQuery(document).ready(function () {
+            jQuery('#designation').val('<?php echo !empty($_GET['designation_id']) ? $_GET['designation_id'] : "";?>');
+        });
 
+        jQuery(document).ready(function () {
+            $.ajax({
+                type: "GET",
+                url: '/stylist/autocomplete',
+                success: function (response) {
+//                    console.log(response);
+                    $( "#searchname" ).autocomplete({
+                        source: response,
+                        minLength: 1,
+                        autofocus:true,
+                        select: function(e, ui) {
+                            console.log(ui.item.value);
+                            $.ajax({
+                                type: "GET",
+                                url: '/stylist/list?searchname=' + ui.item.value,
+                                success: function(){
+                                    location.reload();
+                                }
+                            });
+                        }
+                    });
+                },
+            });
+        });
+    </script>
 @endsection
