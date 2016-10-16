a9fa76a7e03aea0468d185c20a5cbe2dbfea5fb8
avinash57 <avinash.istyleyou@gmail.com>
2016-10-16 15:53:38 +0530

    scraper for Nicobar products

--- backend/stylists/app/Http/Controllers/ScraperController.php
+++ backend/stylists/app/Http/Controllers/ScraperController.php
@@ -103,12 +103,64 @@ class ScraperController extends Controller
                 }
             }
 
-            if($scraperMapperObj->fetchAndSaveProducts($job, $import, $fileObj)){
+            if ($job->spider_id == 13) {
+                $result = $scraperMapperObj->fetchAndSaveNicobarProducts($job, $import, $fileObj);
+            } else {
+                $result = $scraperMapperObj->fetchAndSaveProducts($job, $import, $fileObj);
+            }
+            if($result){
                 $scraperMapperObj->updateJobStatus($job->id);
                 fwrite($fileObj, date("Y-m-d H:i:s"). ' ' .'Import complete for job '. $job->id.PHP_EOL);
             }
         }
         fwrite($fileObj, PHP_EOL.'Successfully imported all items to merchant products'.PHP_EOL);
+
+        fclose($fileObj);
+        return true;
+    }
+
+    public function getFetchNicobar()
+    {
+        if(!$fileObj = fopen(env('LOG_FILE_BASE_PATH').'scraper_log.txt', 'a')){
+            return false;
+        }
+        $scraperMapperObj = new ScraperMapper();
+
+        $spiders = $scraperMapperObj->getSpiders();
+
+        fwrite($fileObj, PHP_EOL.'Initializing... '.PHP_EOL.PHP_EOL);
+
+        foreach ($spiders as $spider) {
+            fwrite($fileObj, date("Y-m-d H:i:s"). ' ' .'Started fetching data for '. $spider->name.PHP_EOL);
+
+            $url = env('NICOBAR_API');
+
+                $job_id_with_dash = preg_replace('/\//', '-', '46');
+                $file_name = $spider->name . '-' . $job_id_with_dash. '-'.strtotime(date("Y-m-d H:i:s")) . '.txt';
+                $write_json_lines = $scraperMapperObj->getContent($url, $file_name);
+
+                if ($write_json_lines['status'] == false) {
+                    fwrite($fileObj, date("Y-m-d H:i:s"). ' ' .'Error fetching data for '. $spider->name.PHP_EOL);
+                    continue;
+                }
+
+                $data = array(
+                    'id' => $spider->merchant_id . '/'.strtotime(date("Y-m-d H:i:s")),
+                    'spider_id' => $spider->id,
+                    'items_scraped' => 10000,
+                    'items_file_path' => $write_json_lines['status'] == true ? $write_json_lines['file_path'] : '',
+                    'items_file_name' => $file_name,
+                    'import_completed' => false,
+                    'created_at' => date('Y-m-d H:i:s'),
+                );
+                $result = $scraperMapperObj->updateJob($data);
+                if ($result == false) {
+                    fwrite($fileObj, date("Y-m-d H:i:s"). ' ' .'Error updating job 46 of '.$spider->name.PHP_EOL);
+                    continue;
+                }
+            fwrite($fileObj, date("Y-m-d H:i:s"). ' ' .'Fetch completed for '. $spider->name.PHP_EOL);
+        }
+        fwrite($fileObj, PHP_EOL.'Complete '.PHP_EOL);
 
         fclose($fileObj);
         return true;
---
+++ backend/stylists/app/Models/Enums/Color.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Models\Enums;
+class Color{
+    const Multi = "4";
+}
+
