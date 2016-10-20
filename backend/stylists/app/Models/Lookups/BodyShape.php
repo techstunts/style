b3198ce60d3b732d50a6e8e8e3fb331dd5e5c697
avinash57 <avinash.istyleyou@gmail.com>
2016-10-20 15:23:42 +0530

    clients information in stylist dashboard shown for IOS and Android users

---
+++ backend/stylists/app/Models/Lookups/BodyShape.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Lookups;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BodyShape extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'lu_body_shape';
+
+}
