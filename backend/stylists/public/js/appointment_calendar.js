684f9dfd27ceff218d74c85e53c856a28b18fca5
Zulqer Nain <zulqer.istyleyou@gmail.com>
2017-02-14 12:05:18 +0530

    calendar changes

--- backend/stylists/public/js/appointment_calendar.js
+++ backend/stylists/public/js/appointment_calendar.js
@@ -3,12 +3,19 @@
  */
 
 
+console.log(moment("20111031", "YYYYMMDD").fromNow())
+
+
 Date.prototype.getWeek = function () {
     var onejan = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 };
 
-// var weekNumber = (new Date()).getWeek();
+var weekNumber = (new Date()).getWeek();
+console.log(weekNumber)
+var currentDateTime;
+var isPast;
+
 //
 // var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 // var now = new Date();
@@ -23,8 +30,11 @@ Date.prototype.getWeek = function () {
 // var weekStartDate = monday.getDate()
 // var weekEndDate = saturday.getDate()
 //document.write(monday+"---"+ weekEndDate)
-var monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+    "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+];
+var days = ["Mon", "Tue", "Wed", "Thr", "Fri", "Sat",
+    "Sun"
 ];
 var seesionInHour = 2;
 var minutInOneSession = 60 / seesionInHour;
@@ -42,29 +52,41 @@ function renderCal() {
 
 
         var current = new Date();     // get current date
-        if (current.getMonth() == 11) {
-            current = new Date(current.getFullYear() + 1, 0, 1);
-        } else {
-            current = new Date(current.getFullYear(), current.getMonth() + 1, 1);
-        }
+
         var weekstart = current.getDate() - current.getDay() + dayAhead;
         var weekend = weekstart + i;       // end day is the first day + 6
         var monday = new Date(current.setDate(weekstart));
+
+        // if (current.getMonth() == 11) {
+        //     current = new Date(current.getFullYear() + 1, 0, 1);
+        // } else {
+        //     current = new Date(current.getFullYear(), current.getMonth() + 1, 1);
+        // }
         var nextDay = new Date(current.setDate(weekend));
         var nextDate = nextDay.getDate()
         var month = parseInt(nextDay.getUTCMonth())
         var day = calData.days[i]
+        var arrrr = day.date.split('-')
+        var apiDate = arrrr[0]
+        var apiMonth = arrrr[1]
         var slots = day.slots
 
         calHtml += '<div class="col-md-2">'
 
-        // document.write(nextDate+" ")
-        // document.write(monthNames[month] )
-        // document.write("<br>")
-        calHtml += '<h3 class="text-center cal-header">' + nextDate + " " + monthNames[month] + '</h3>'
+
+        var editable = 'non-editable'
+        var iseditable = false;
+
+        if (new Date() < nextDay) {
+            iseditable = true
+        }
+        if (iseditable) {
+            editable = 'editable'
+        }
+        calHtml += '<h4 class="text-center cal-header">' + days[i] + " " + apiDate + " " + monthNames[parseInt(apiMonth) - 1] + '</h4>'
         var x = 0
         for (var j = 10; j < 17; j++) {
-            calHtml += '<div class="row text-center cal-time-container">';
+            // calHtml += '<div class="row text-center cal-time-container">';
             for (k = 0; k < seesionInHour; k++) {
                 var slot = slots[x];
 
@@ -75,9 +97,9 @@ function renderCal() {
                 // name=name[0]
                 // console.log(name)
                 x++
-                var isVisible = 'invisible'
+                var isVisible = 'visible'
                 var slotAvailable = 'slotAvailable'
-                if (!available) {
+                if (stylists.length>0) {
                     isVisible = 'visible'
                     slotAvailable = 'slotUnAvailable'
                 }
@@ -88,10 +110,18 @@ function renderCal() {
                 var upto = minutInOneSession * (k + 1)
                 from = j + ':' + from
                 upto = j + ':' + upto
-                calHtml += '<div class=" cal-time-button ' + slotAvailable + '" available=' + available + ' date="' + day.date + '"   slot="' + name + '" slot_id="' + slot.id + '"  ><div class="round-btn-selected ' + isVisible + '">' + stylists.length + '</div><div class="timeSelected">' + name + '</div> </div>';
+                var initials = ''
+                for (var z = 0; z < stylists.length; z++) {
+                    var s_name = stylists[z].name
+                    s_name = s_name.trim()
+                    var initialChar = s_name.substring(0, 4)
+                    initials += '<div class="round-btn-selected ' + isVisible + '">' + initialChar + '</div>'
+                }
+
+                calHtml += '<div class=" cal-time-button ' + slotAvailable + ' ' + editable + ' " isEditable=' + editable + ' available=' + available + ' date="' + day.date + '"   slot="' + name + '" slot_id="' + slot.id + '"  >' + initials + '</div>';
 
             }
-            calHtml += '</div>';
+            // calHtml += '</div>';
         }
         calHtml += '</div>'
 
@@ -100,10 +130,15 @@ function renderCal() {
     $('.cal').html(calHtml)
     $('.zcal').addClass('animated ');
     $('.cal-time-button').on('click', function () {
+
         var date = this.getAttribute("date")
         var slot = this.getAttribute("slot")
         var slot_id = this.getAttribute("slot_id")
         var available = this.getAttribute("available")
+        var isEditable=this.getAttribute("isEditable")
+        if (isEditable=="non-editable"){
+            return false;
+        }
         if (available == 'true') {
             $(this).removeClass('slotAvailable')
             $(this).addClass('slotUnAvailable')
@@ -112,18 +147,42 @@ function renderCal() {
             obj.date = date
             obj.slot_id = slot_id
 
-            selectedSlots.push(obj)
+            for (i in selectedSlots) {
+                if (selectedSlots[i].date == date && selectedSlots[i].slot_id == slot_id) {
+                    selectedSlots.splice(i, 1)
+                    availableInSelectedSlot = true
+                    break;
+                }
+            }
+            if (!availableInSelectedSlot) {
+                var obj = {}
+                obj.date = date
+                obj.slot_id = slot_id
+
+                selectedSlots.push(obj)
+            }
         } else if (available == 'false') {
             $(this).removeClass('slotUnAvailable')
             $(this).addClass('slotAvailable')
             this.setAttribute("available", true);
+            var availableInSelectedSlot = false;
             for (i in selectedSlots) {
                 if (selectedSlots[i].date == date && selectedSlots[i].slot_id == slot_id) {
                     selectedSlots.splice(i, 1)
+                    availableInSelectedSlot = true
                     break;
                 }
             }
+            if (!availableInSelectedSlot) {
+                var obj = {}
+                obj.date = date
+                obj.slot_id = slot_id
+
+                selectedSlots.push(obj)
+            }
         }
+        console.log(selectedSlots)
+        saveButtonUpdate()
     });
 }
 
@@ -133,7 +192,7 @@ getslots('nextWeek');
 $('#nextWeek').on('click', function () {
     if (isSelctedSlots()) {
 
-        var aa = confirm('there is unsaved data do you want tp proceed');
+        var aa = confirm('there is unsaved data do you want to proceed');
         if (aa) {
             selectedSlots = [];
             dayAhead = dayAhead + 7;
@@ -171,28 +230,36 @@ $('#prevWeek').on('click', function () {
 });
 $('#save').on('click', saveSelected);
 
-function getslots(week)
-{
+function getslots(week) {
+    saveButtonUpdate()
     checkStylist();
     var now = new Date();     // get current date
-
+    console.log(now)
+    currentDateTime = now
     var weekstart = now.getDate() - now.getDay() + dayAhead;
     var weekend = weekstart + 5;       // end day is the first day + 6
     var monday = new Date(now.setDate(weekstart));
     if (now.getMonth() == 11) {
         now = new Date(now.getFullYear() + 1, 0, 1);
     } else {
-        now = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+        //now = new Date(now.getFullYear(), now.getMonth() + 1, 1);
     }
     var nextDay = new Date(now.setDate(weekend));
+    // console.log(monday + "    " + nextDay)
+    // return false;
     // var nextDate = nextDay.getDate()
     // var month = parseInt(nextDay.getUTCMonth())
 
 
-    console.log(monday)
-    console.log(nextDay)
-    var startMonth = monday.getMonth() + 1
+    // console.log(monday)
+    // console.log(nextDay)
+    var startMonth = monday.getMonth()
     var endMonth = nextDay.getMonth() + 1
+    if (dayAhead < 0) {
+        endMonth = nextDay.getMonth() + 1
+    } else {
+        startMonth = monday.getMonth() + 1
+    }
 
     var startDate = monday.getDate() + "-" + startMonth + '-' + monday.getFullYear()
     var endtDate = nextDay.getDate() + "-" + endMonth + '-' + nextDay.getFullYear()
@@ -217,30 +284,42 @@ function getslots(week)
 }
 
 function saveSelected(e) {
-    checkStylist();
-    $.ajax({
-        type: "POST",
-        url: api_origin + '/stylist/availability',
-        data: {
-            'avail_dt_slot': selectedSlots,
-            'stylist_id': stylist_id
-        },
-        success : function (response) {
-            if (response[0].status == false)
-                alert(response[0].errorMsg);
-            else
-                alert(response[0].message);
-            console.log(response);
-        }
-    });
-    e.preventDefault();
+
+    if (checkStylist()) {
+        $.ajax({
+            type: "POST",
+            url: api_origin + '/stylist/availability',
+            data: {
+                'avail_dt_slot': selectedSlots,
+                'stylist_id': stylist_id
+            },
+            success: function (response) {
+                if (response.status == false)
+                    alert(response.errorMsg);
+
+                else
+                    alert(response.message);
+                selectedSlots = []
+                saveButtonUpdate()
+
+                getslots();
+
+                console.log(response);
+            }
+        });
+        e.preventDefault();
+    }
+
 }
 
 function checkStylist() {
     if (stylist_id === '') {
         alert('Something wrong');
         return false;
+    } else if (selectedSlots.length == 0) {
+        return false;
     }
+    return true;
 }
 
 function isSelctedSlots() {
@@ -248,4 +327,27 @@ function isSelctedSlots() {
         return true
     }
     return false
+}
+function saveButtonUpdate() {
+    if (selectedSlots.length > 0) {
+        $("#save").removeClass('disabled-btn')
+
+        $("#save").addClass('selected-btn')
+    } else {
+        $("#save").removeClass('selected-btn')
+
+        $("#save").addClass('disabled-btn')
+
+    }
+}
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [day, month, year].join('-');
 }
--- backend/stylists/resources/views/stylist/calendar.blade.php
+++ backend/stylists/resources/views/stylist/calendar.blade.php
@@ -1,14 +1,65 @@
 @extends('layouts.master')
 
-@section('title', 'ashjdfgjshadfjgjsg')
+@section('title', 'Stylist Calendar')
 
 @section('content')
     <div id="contentCntr">
         <div class="container">
             <div class="calendar-container">
-                <div class="cal">
+                <div class="row">
+                    <div class="col-md-1"><h4 class="text-center cal-header">Slot</h4>
+                        <div class=" cal-range-button">
+                            <div class="timeSelected">10:00 - 10:30</div>
+                        </div>
+                        <div class=" cal-range-button" >
+                            <div class="timeSelected">10:30 - 11:00</div>
+                        </div>
+                        <div class=" cal-range-button">
+                            <div class="timeSelected">11:00 - 11:30</div>
+                        </div>
+                        <div class=" cal-range-button" >
+                            <div class="timeSelected">11:30 - 12:00</div>
+                        </div>
+                        <div class=" cal-range-button" >
+                            <div class="timeSelected">12:00 - 12:30</div>
+                        </div>
+                        <div class=" cal-range-button" >
+                            <div class="timeSelected">12:30 - 13:00</div>
+                        </div>
+                        <div class="cal-range-button" >
+                            <div class="timeSelected">13:00 - 13:30</div>
+                        </div>
+                        <div class=" cal-range-button" >
+                            <div class="timeSelected">13:30 - 14:00</div>
+                        </div>
+                        <div class="cal-range-button" >
+                            <div class="timeSelected">14:00 - 14:30</div>
+                        </div>
+                        <div class="cal-range-button" >
+                            <div class="round-btn-selected invisible">0</div>
+                            <div class="timeSelected">14:30 - 15:00</div>
+                        </div>
+                        <div class=" cal-range-button" >
+                            <div class="round-btn-selected invisible">0</div>
+                            <div class="timeSelected">15:00 - 15:30</div>
+                        </div>
+                        <div class="cal-range-button">
+                            <div class="timeSelected">15:30 - 16:00</div>
+                        </div>
+                        <div class=" cal-range-button" >
+                            <div class="timeSelected">16:00 - 16:30</div>
+                        </div>
+                        <div class=" cal-range-button" >
+                            <div class="timeSelected">16:30 - 17:00</div>
+                        </div>
+                    </div>
+                    <div class="col-md-11">
+                        <div class="cal">
 
+                        </div>
+                    </div>
                 </div>
+
 
             </div>
         </div>
@@ -20,13 +71,14 @@
                 <div id="prevWeek" class="selected-btn nav-btn-text"><b>PREV</b></div>
             </div>
             <div class=" col-lg-offset-1 col-md-3">
-                <div id="save" class="selected-btn nav-btn-text"><b>SAVE</b></div>
+                <div id="save" class="disabled-btn nav-btn-text"><b>SAVE</b></div>
             </div>
             <div class=" col-lg-offset-1 col-md-3">
                 <div id="nextWeek" class="selected-btn nav-btn-text"><b>NEXT</b></div>
             </div>
         </div>
     </div>
+    <script type="text/javascript" src="http://momentjs.com/downloads/moment.js"></script>
 
     <script type="text/javascript" src="{!! asset('js/appointment_calendar.js') !!}"></script>
 @endsection
