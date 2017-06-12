e51fd55444a3caa29f2980bba14e0978b410dd24
danishahmed73 <danishahmed73@gmail.com>
2017-06-12 16:58:23 +0530

    Emailer : Change in footer design.

--- backend/stylists/resources/views/emails/booking_reminder.blade.php
+++ backend/stylists/resources/views/emails/booking_reminder.blade.php
@@ -58,132 +58,138 @@
             </table>
         </td>
     </tr>
+
     <tr>
         <td style="background:#e5eff1;margin-top:10px;">
             <img style="width: 100%" src="{{$static_url}}styling/emailer/Appointment/kalash-for-booking.jpg" alt="" class="CToWUd">
         </td>
     </tr>
+
     <tr>
-        <td style="background:#e5eff1;padding-bottom:20px;">
-            <table border="0" cellpadding="8" cellspacing="0" style="width:500px;margin:0 auto;text-align:center;font-size:11px;letter-spacing:.08em;">
-                <tbody>
+        <td style="padding-top:20px; padding-bottom:20px; padding-left:10%; padding-right:10%;background: #f7f7f6">
+            <table style="width: 100%;" cellpadding="0" cellspacing="0">
                 <tr>
-                    <td>
-                        <a href="#m_-7481920367755743928_" style="font-weight:bold;text-decoration:none;color:#282b30;">#nicobar</a>
-                        <br><a href="#m_-7481920367755743928_" style="font-weight:bold;text-decoration:none;color:#282b30;">#thenicobarstory</a>
+                    <td colspan="4" style="text-align: left;"><img src="{{$static_url}}styling/emailer/footer/our_stores.png" alt="" style="border: 0;"> </td>
+                </tr>
+                <tr>
+                    <td colspan="4" style="padding-top: 20px; text-align: left; padding-bottom: 20px;"><a href="https://www.nicobar.com/delhi-store/" target="_blank"><img src="{{$static_url}}styling/emailer/footer/stores-img.png" alt="" style="border: 0; width: 100%; max-width: 100%;"></a></td>
+                </tr>
+                <tr>
+                    <td style="text-align: left"><a href="https://www.nicobar.com/mumbai-store/" target="_blank"><img src="{{$static_url}}styling/emailer/footer/store_mumbai.png" alt="" style="border: 0;"></a></td>
+                    <td><a href="https://www.nicobar.com/delhi-store/" target="_blank"><img src="{{$static_url}}styling/emailer/footer/store_delhi.png" alt="" style="border: 0;"></a></td>
+                    <td><a href="https://www.nicobar.com/bangalore-store/" target="_blank"><img src="{{$static_url}}styling/emailer/footer/store_bangalore.png" alt="" style="border: 0;"></a></td>
+                    <td style="text-align: right"><a href="https://www.nicobar.com/jodhpur-store/" target="_blank"><img src="{{$static_url}}styling/emailer/footer/store_jodhpur.png" alt="" style="border: 0;"></a></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+
+    <tr><td style="padding-bottom:30px;background:#f7f7f6;">&nbsp;</td></tr>
+
+    <tr>
+        <td style="color: #ffffff; padding-top:20px; padding-bottom:25px; padding-left:8%; padding-right:8%; background-image: url('{{$static_url}}styling/emailer/footer/footer_bg.png'); background-size: cover;">
+            <table border="0" cellpadding="0" cellspacing="0"
+                   style="width:100%; margin:0 auto; text-align:center; font-size:11px; letter-spacing:0.025em;">
+                <tr>
+                    <td><img src="{{$static_url}}styling/emailer/footer/nicobar_icon.png" alt=""></td>
+                </tr>
+                <tr>
+                    <td style="padding-top: 12px;"><img src="{{$static_url}}styling/emailer/footer/design-for-everyday-living.png" alt=""></td>
+                </tr>
+                <tr>
+                    <td style="border-bottom: 1px solid rgba(255, 255, 255, 0.5); font-size: 0; height: 10px; line-height: 0; padding-top: 3px; margin: 0;">
+                        &nbsp;
                     </td>
                 </tr>
                 <tr>
-                    <td style="padding-top:4px;font-style:italic;font-size:9px;">
-                        Need help? Have a question about an order,<br>or about getting in touch? <br>We're always happy
-                        to hear from you.
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-top:4px;" valign="top">
-                        <img src="{{$static_url}}styling/emailer/common/phone-icon.png" alt="" style="border:0" class="CToWUd">    
-                        <img src="{{$static_url}}styling/emailer/common/mail-icon.png" alt="" style="border:0" class="CToWUd">
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-top:4px;">
-                        <a href="tel:+918588000150" style="text-decoration:none;color:#2c2e2e;" target="_blank">+91
-                            22 2263 3888</a> &amp; <a href="tel:+918588000151" style="text-decoration:none;color:#2c2e2e;" target="_blank">+91 22
-                            2263 3877</a>
-                        <br><a href="mailto:care@nicobar.com" style="text-decoration:none;color:#2c2e2e;" target="_blank">care@nicobar.com</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-top:4px;font-size:12px;text-transform:uppercase;font-weight:bold;">
-                        Nicobar Design Studio
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-top:0px;font-size:11px;line-height:1.6;">
-                        <table border="0" cellpadding="0" cellspacing="0" style="width:500px;margin:0 auto;text-align:center;font-size:11px;letter-spacing:.08em;">
-                            <tbody>
+                    <td style="padding-top: 10px;">
+                        <table cellspacing="0" cellpadding="0" border="0" style="">
                             <tr>
-                                <td style="text-align:right;" width="245">
-                                    <address style="font-style:normal;font-size:1em;">
-                                        Above Kala Ghoda Cafe,<br>
-                                        10, Ropewalk Lane<br>
-                                        Kala Ghoda Fort,<br>
-                                        Mumbai - 400001<br>
-                                    </address>
+                                <td style="text-align: left; width: 40%" valign="top">
+                                    <p style="font-weight: bold">Store Timings</p>
+                                    <p style="text-transform: uppercase; margin: 0; padding-top: 2px; line-height: 1.6;">Mon -
+                                        sun | 11:00 am - 8:00 PM (ist)</p>
+                                    <p style="padding-top: 7px;">
+                                        <a href="https://www.facebook.com/nicobarstudio" target="_blank"
+                                           style="display: inline-block; margin-right:7px; padding-right:7px;"><img
+                                                    src="{{$static_url}}styling/emailer/footer/facebook-white-icon.png"
+                                                    alt=""></a>
+                                        <a href="https://www.instagram.com/nicojournal" target="_blank"
+                                           style="display: inline-block; margin: 0 7px; padding: 0 7px;"><img
+                                                    src="{{$static_url}}styling/emailer/footer/instagram-white-icon.png"
+                                                    alt=""></a>
+                                        <a href="https://in.pinterest.com/nicojournal/" target="_blank"
+                                           style="display: inline-block; margin: 0 7px; padding: 0 7px;"><img
+                                                    src="{{$static_url}}styling/emailer/footer/pinterest-white-icon.png"
+                                                    alt=""></a>
+                                        <a href="tel:+918588000150" target="_blank" style="display: inline-block; margin: 0 7px; padding: 0 7px;"><img
+                                                    src="{{$static_url}}styling/emailer/footer/phone-white-icon.png"
+                                                    alt=""></a>
+                                        <a href="mailto:care@nicobar.com" target="_blank" style="display: inline-block; margin-left:7px; padding-left: 7px;"><img
+                                                    src="{{$static_url}}styling/emailer/footer/mail-white-icon.png"
+                                                    alt=""></a>
+                                    </p>
                                 </td>
-                                <td style="width:10px;border-right:1px solid #6d6d6d;">
-                                </td>
-                                <td style="text-align:center;" width="245">
-                                    <address style="font-style:normal;font-size:1em;">
-                                        Shop #79 &amp; 80, <br>
-                                        Above Diva Spiced,Meherchand Market,<br>
-                                        Fifth Avenue Road<br>
-                                        Lodhi Colony, 110003<br>
-                                    </address>
-                                </td>
-                                <td style="width:10px;border-right:1px solid #6d6d6d;">
-                                </td>
-                                <td style="text-align:left;padding-left:10px;" width="245">
-                                    <address style="font-style:normal;font-size:1em;">
-                                        Above Good Earth No. 4,<br>
-                                        Walton Road<br>
-                                        Off Lavelle Road<br>
-                                        Banglore - 560001<br>
-                                    </address>
+                                <td width="3.5%;" style="border-right: 1px solid rgba(255, 255, 255, 0.5);"></td>
+                                <td style="text-align: left; padding-left: 8%; width: 42%" valign="top">
+                                    <p style="font-weight: bold">Customer Care</p>
+                                    <p style="text-transform: uppercase; margin: 0; padding-top: 2px; line-height: 1.6;">Mon -
+                                        sat | 9:00 am - 6:00 PM (ist)</p>
+                                    <p style="margin: 0; padding-top: 3px; line-height: 1.6;">
+                                        <a href="tel:+918588000150" style="text-decoration:none; color:#ffffff;">+ 91
+                                            8588000150</a> |
+                                        <a href="tel:+918588000151" style="text-decoration:none; color:#ffffff;">+ 91
+                                            8588000151</a>
+                                    </p>
+                                    <p style="margin: 0; padding: 0; line-height: 1.6;"><a
+                                                href="mailto:care@nicobar.com" style="text-decoration:none; color:#ffffff;">care@nicobar.com</a>
+                                    </p>
                                 </td>
                             </tr>
-                            </tbody>
                         </table>
                     </td>
                 </tr>
                 <tr>
-                    <td style="padding-top:4px;font-size:11px;">
-                        <a href="https://www.facebook.com/nicobarstudio" style="text-decoration:none;color:#2c2e2e;" target="_blank">
-                            <img src="{{$static_url}}styling/emailer/common/fb-icon.png" alt="" class="CToWUd"> /nicobarstudio</a>   
-                        <a href="https://www.instagram.com/nicojournal" style="text-decoration:none;color:#2c2e2e;" target="_blank">
-                            <img src="{{$static_url}}styling/emailer/common/instagram-icon.png" alt="" class="CToWUd"> /nicojournal</a>
+                    <td style="border-bottom: 1px solid rgba(255, 255, 255, 0.5); font-size: 0; line-height: 0; margin: 0; padding: 0; padding-top: 10px;">
+                        &nbsp;
                     </td>
                 </tr>
                 <tr>
-                    <td style="padding-top:10px;text-transform:uppercase;font-weight:bold;font-size:9px;line-height:2;">
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Women</a>
-                        |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Men</a>
-                        |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">House
-                            &amp; Home</a> |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Travel</a>
-                        |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Journal</a>
-                        |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Story</a>
-                        |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">In
-                            The Press</a> |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Contact
-                            us</a> |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Careers</a>
-                        |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Shipping
-                            &amp; Returns</a> |
-                        <a href="#" style="text-decoration:none;color:#2c2e2e;" target="_blank">Terms
-                            &amp; Conditions</a>
+                    <td style="padding-top:15px;  line-height: 1.6;">
+                        <a href="https://www.nicobar.com/philosophy/" target="_blank"
+                           style="text-decoration:none; color:#ffffff;">About us</a><span style="padding-left: 18px; padding-right: 18px; display: inline-block">|</span>
+                        <a href="https://www.nicobar.com/philosophy-details/" target="_blank"
+                           style="text-decoration:none; color:#ffffff;">Design</a><span style="padding-left: 18px; padding-right: 18px; display: inline-block">|</span>
+                        <a href="https://www.nicobar.com/press-stories/" target="_blank"
+                           style="text-decoration:none; color:#ffffff;">In the press</a><span style="padding-left: 18px; padding-right: 18px; display: inline-block">|</span>
+                        <a href="https://www.nicobar.com/nico-tides/" target="_blank"
+                           style="text-decoration:none; color:#ffffff;">Nicotides</a><span style="padding-left: 18px; padding-right: 18px; display: inline-block">|</span>
+                        <a href="https://www.nicobar.com/delhi-store/" target="_blank"
+                           style="text-decoration:none; color:#ffffff;">Stores</a><span style="padding-left: 18px; padding-left: 18px; display: inline-block">|</span>
+                        <a href="https://www.nicobar.com/care/" target="_blank"
+                           style="text-decoration:none; color:#ffffff; padding-left: 18px;">Contact us</a>
                     </td>
                 </tr>
                 <tr>
-                    <td style="padding-top:30px;font-weight:bold;font-size:9px;line-height:1;">
-                        <a href="https://www.nicobar.com" style="text-decoration:none;color:#2c2e2e;" target="_blank">www.nicobar.com</a>
+                    <td style="padding-top:3px; line-height: 1.6;">
+                        <a href="https://www.nicobar.com/careers/" target="_blank"
+                           style="text-decoration:none; color:#ffffff;">We're hiring</a><span style="padding-left: 18px; padding-right: 18px; display: inline-block">|</span>
+                        <a href="https://www.nicobar.com/shipping-returns/" target="_blank"
+                           style="text-decoration:none; color:#ffffff;">Shipping & Returns</a><span style="padding-left: 17px; padding-left: 18px; padding-right: 18px; display: inline-block">|</span>
+                        <a href="https://www.nicobar.com/tnc/" target="_blank"
+                           style="text-decoration:none; color:#ffffff;">Terms & Conditions</a>
                     </td>
                 </tr>
                 <tr>
-                    <td style="padding-top:0px;font-size:8px;line-height:1;">
-                        Nicobar All Rights Reserved � 2016
+                    <td style="font-size:9px; padding-top: 18px; line-height:1.6;">
+                        <a href="https://www.nicobar.com" target="_blank" style="text-decoration:none; color:#ffffff;">Nicobar
+                            Design Studio</a>. Nicobar All Rights Reserved &copy; 2017
                     </td>
                 </tr>
-                </tbody>
             </table>
         </td>
     </tr>
+
     </tbody>
 </table>
 </body>
