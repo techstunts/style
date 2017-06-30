f8e0d3612bc202cbc5b4d07fecbe929d5007bff9
avinash57 <avinash.istyleyou@gmail.com>
2017-06-30 13:29:04 +0530

    showing client, request and booking list for designers

--- backend/stylists/resources/views/layouts/menu.blade.php
+++ backend/stylists/resources/views/layouts/menu.blade.php
@@ -21,6 +21,9 @@
                     @elseif (0 === strrpos(\Illuminate\Support\Facades\Request::getHost(), 'designer'))
                         <li><a href="/client/chat">Chat</a></li>
                         <li><a href="/product/list">Products</a></li>
+                        <li><a href="/requests/list">Requests</a></li>
+                        <li><a href="/bookings/list">Bookings</a></li>
+                        <li><a href="/client/list">Clients</a></li>
                         <li class="pull-right" a><a href="{!! url('auth/logout') !!}">Logout</a></li>
                         <li class="pull-right"><a href="{!! url('stylist/view/' . Auth::user()->id) !!}">{{Auth::user()->name}}</a></li>
                     @else
