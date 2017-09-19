027a06d8119c3729395dd6e31af06d1ca405eb76
avinash57 <avinash.istyleyou@gmail.com>
2017-09-19 15:23:33 +0530

    database config changes

--- backend/stylists/config/database.php
+++ backend/stylists/config/database.php
@@ -83,7 +83,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
-            'prefix'   => '',
+            'prefix'   => env('DB_PREFIX', ''),
             'schema'   => 'public',
         ],
 
